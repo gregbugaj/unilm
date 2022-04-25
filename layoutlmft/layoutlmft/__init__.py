3f2be6c45178a9865a5356cc2e333d8a5e1bbfae
Greg Bugaj <gregbugaj@yahoo.com>
2022-04-25 11:20:52 -0500

    Fix issue ImportError: cannot import name 'auto_class_factory'

--- layoutlmft/layoutlmft/__init__.py
+++ layoutlmft/layoutlmft/__init__.py
@@ -1,8 +1,14 @@
 from collections import OrderedDict
+import types
 
 from transformers import CONFIG_MAPPING, MODEL_FOR_TOKEN_CLASSIFICATION_MAPPING, MODEL_NAMES_MAPPING, TOKENIZER_MAPPING
 from transformers.convert_slow_tokenizer import SLOW_TO_FAST_CONVERTERS, BertConverter, XLMRobertaConverter
-from transformers.models.auto.modeling_auto import auto_class_factory
+
+# https://github.com/huggingface/transformers/issues/13591
+try:
+    from transformers.models.auto.modeling_auto import auto_class_factory
+except:
+    from transformers.models.auto.modeling_auto import _BaseAutoModelClass, auto_class_update
 
 from .models.layoutlmv2 import (
     LayoutLMv2Config,
@@ -37,10 +43,24 @@ MODEL_FOR_RELATION_EXTRACTION_MAPPING = OrderedDict(
     [(LayoutLMv2Config, LayoutLMv2ForRelationExtraction), (LayoutXLMConfig, LayoutXLMForRelationExtraction)]
 )
 
-AutoModelForTokenClassification = auto_class_factory(
-    "AutoModelForTokenClassification", MODEL_FOR_TOKEN_CLASSIFICATION_MAPPING, head_doc="token classification"
-)
 
-AutoModelForRelationExtraction = auto_class_factory(
-    "AutoModelForRelationExtraction", MODEL_FOR_RELATION_EXTRACTION_MAPPING, head_doc="relation extraction"
-)
+try:
+    AutoModelForTokenClassification = auto_class_factory(
+        "AutoModelForTokenClassification", MODEL_FOR_TOKEN_CLASSIFICATION_MAPPING, head_doc="token classification")
+except:
+    cls = types.new_class("AutoModelForTokenClassification", (_BaseAutoModelClass,))
+    cls._model_mapping = MODEL_FOR_TOKEN_CLASSIFICATION_MAPPING
+    cls.__name__ = "AutoModelForTokenClassification"
+
+    AutoModelForTokenClassification = auto_class_update(cls, head_doc="token classification")
+
+
+try:
+    AutoModelForRelationExtraction = auto_class_update(
+        "AutoModelForRelationExtraction", MODEL_FOR_RELATION_EXTRACTION_MAPPING, head_doc="relation extraction")
+except:
+    cls = types.new_class("AutoModelForRelationExtraction", (_BaseAutoModelClass,))
+    cls._model_mapping = MODEL_FOR_TOKEN_CLASSIFICATION_MAPPING
+    cls.__name__ = "AutoModelForRelationExtraction"
+
+    AutoModelForRelationExtraction = auto_class_update(cls, head_doc="relation extraction")
