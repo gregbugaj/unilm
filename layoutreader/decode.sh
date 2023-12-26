09c418356f6d7f0a972f0a48236edd7f060924fa
Greg <gbugaj@rmsweb.com>
2023-12-26 17:24:41 -0600

    wip: decoting s2s

--- layoutreader/decode.sh
+++ layoutreader/decode.sh
@@ -4,16 +4,17 @@ export CUDA_VISIBLE_DEVICES=0
 export OMP_NUM_THREADS=4
 export MKL_NUM_THREADS=4
 
+# --cached_feature_file /path/to/ReadingBank/features_test.pt \
+
 python decode_seq2seq.py --fp16 \
     --model_type layoutlm \
     --tokenizer_name bert-base-uncased \
-    --input_folder /path/to/ReadingBank/test \
-    --cached_feature_file /path/to/ReadingBank/features_test.pt \
-    --output_file /path/to/output/LayoutReader/layoutlm/output.txt \
+    --input_folder /mnt/data/marie-ai/model_zoo/unilm/layoutreader/ReadingBank/fast \
+    --output_file /mnt/data/marie-ai/model_zoo/unilm/layoutreader/output.txt \
     --split test \
     --do_lower_case \
-    --model_path /path/to/output/LayoutReader/layoutlm/ckpt-75000 \
-    --cache_dir /path/to/output/LayoutReader/cache \
+    --model_path /mnt/data/marie-ai/model_zoo/unilm/layoutreader/layoutreader-base-readingbank \
+    --cache_dir /mnt/data/marie-ai/model_zoo/unilm/layoutreader/cache \
     --max_seq_length 1024 \
     --max_tgt_length 511 \
     --batch_size 32 \
