9e4610068acc7c8671fd8ed3346d4e58771b9f55
Greg <gregbugaj@yahoo.com>
2024-04-22 07:21:10 -0500

    wip

--- trocr/convert_dir_lines_to_SROIE_format.py
+++ trocr/convert_dir_lines_to_SROIE_format.py
@@ -27,28 +27,33 @@ def process_dir(src_dir: str, output_dir:str) -> None:
     img_dirs = glob.glob(os.path.join(src_dir, "**"))
 
 
-    # Split the image directories into training and testing sets
-    train_dirs, test_dirs = train_test_split(img_dirs, test_size=0.2, random_state=42)
-
     for idx, img_dir in enumerate(img_dirs):
         try:
             print("Processing: ", img_dir)
             files = os.listdir(img_dir)
             img_files = [f for f in files if f.endswith('.png')]
-            label_file = [f for f in files if f.endswith('.txt')][0]
 
-            print("Label file: ", label_file)
             print("Image files: ", img_files)
 
-            with open(os.path.join(img_dir, label_file), 'r') as f:
-                labels = f.readlines()
-
-            text = labels[0].strip()
 
             for img_file in img_files:
+                print("img_file: ", img_file)
                 src_image = os.path.join(img_dir, img_file)
                 img = Image.open(src_image)
+
+                label_file = img_file.replace('.png', '.txt')
+                with open(os.path.join(img_dir, label_file), 'r') as f:
+                    labels = f.readlines()
+
+                text = labels[0].strip()
                 save_dir = output_dir
+
+                if text == '':
+                    raise Exception(f"Empty text found in label file. : {label_file}")
+                
+                # Convert image to RGB if it's RGBA
+                if img.mode == 'RGBA':
+                    img = img.convert('RGB')
 
                 img.save(os.path.join(save_dir, img_file.replace('.png', '.jpg')), quality=100)
 
@@ -63,41 +68,13 @@ def process_dir(src_dir: str, output_dir:str) -> None:
         except Exception as e:
             print(e)
 
-def split_dir(src_dir, output_dir):
-    
-    # Create directories for the training and testing sets
-    train_dir = os.path.join(output_dir, 'train')
-    test_dir = os.path.join(output_dir, 'test')
-    os.makedirs(train_dir, exist_ok=True)
-    os.makedirs(test_dir, exist_ok=True)
-    
-    img_dirs = glob.glob(os.path.join(src_dir, "*.jpg"))
-    random.shuffle(img_dirs)
-
-    train_dirs, test_dirs = train_test_split(img_dirs, test_size=0.2, random_state=42)
-    # Copy the images and labels to the training and testing directories
-    for img_dir in train_dirs:
-        shutil.copy(img_dir, train_dir)
-        shutil.copy(img_dir.replace('.jpg', '.txt'), train_dir)
-
-    for img_dir in test_dirs:
-        shutil.copy(img_dir, test_dir)
-        shutil.copy(img_dir.replace('.jpg', '.txt'), test_dir)
-    
-
+ 
 
 if __name__ == "__main__":
 
-    src_dir = '/home/greg/datasets/SROIE_OCR/boxes/validated-True/alpha'
-    src_dir = '/home/greg/datasets/SROIE_OCR/boxes/validated-True/number'
-
-    src_dir = '/home/greg/datasets/SROIE_OCR/raw'
-    
-    output_dir = '/home/greg/datasets/SROIE_OCR/converted'
-    output_train_test_dir = '/home/greg/datasets/SROIE_OCR/ready'
-
-    # process_dir(src_dir, output_dir)
-
-    split_dir(output_dir, output_train_test_dir)
+    src_dir = '/home/greg/datasets/SROIE_OCR/lines/raw'
+    # src_dir = '/home/greg/datasets/SROIE_OCR/raw'
+    output_dir = '/home/greg/datasets/SROIE_OCR/lines/converted'
+    process_dir(src_dir, output_dir)
     
 
--- trocr/finetune_sroie.sh
+++ trocr/finetune_sroie.sh
@@ -3,22 +3,22 @@ export DATA=/home/greg/datasets/SROIE_FineTune
 export DATA=/home/greg/datasets/SROIE_OCR/ready
 export MODEL_BASE=/mnt/data/marie-ai/model_zoo/trocr/trocr-large-printed.pt
 
-export MODEL_NAME=ft_SROIE
-export SAVE_PATH=/home/greg/models/unilm/trocr/${MODEL_NAME}
+export MODEL_NAME=ft_SROIE_LINES_SET13
+export SAVE_PATH=/data/models/unilm/trocr/${MODEL_NAME}
 export LOG_DIR=log_${MODEL_NAME}
 
 mkdir ${LOG_DIR}
-export BSZ=6
-export valid_BSZ=6
+export BSZ=4
+export valid_BSZ=4
 
 $(which fairseq-train) \
     --data-type SROIE --user-dir ./ --task text_recognition --input-size 384 \
     --arch trocr_large \
-    --seed 1111 --optimizer adam --lr 5e-05 --lr-scheduler inverse_sqrt \
+    --seed 1234 --optimizer adam --lr 2e-05 --lr-scheduler inverse_sqrt \
     --warmup-init-lr 1e-8 --warmup-updates 800 --weight-decay 0.0001 --log-format tqdm \
     --log-interval 10 --batch-size ${BSZ} --batch-size-valid ${valid_BSZ} \
-    --save-dir ${SAVE_PATH} --tensorboard-logdir ${LOG_DIR} --max-epoch 300 \
-    --patience 10 --ddp-backend legacy_ddp --num-workers 10 --preprocess DA2 \
+    --save-dir ${SAVE_PATH} --tensorboard-logdir ${LOG_DIR} --max-epoch 5000 \
+    --patience 16 --ddp-backend legacy_ddp --num-workers 10 --preprocess DA2 \
     --bpe gpt2 --decoder-pretrained roberta2 \
     --update-freq 16 --finetune-from-model ${MODEL_BASE} --fp16 \
     ${DATA}
